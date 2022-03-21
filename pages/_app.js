11be0e42f45ef3283d37a6fe6f5b785b0af8d5e9
bryceparkman <brycecparkman@gmail.com>
2022-03-21 13:36:52 -0400

    Added Bootstrap to project

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 
+import { useEffect } from "react";
+
 function MyApp({ Component, pageProps }) {
+  //Make sure that the window and document objects will only be used on the client side
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
   return <Component {...pageProps} />
 }
 

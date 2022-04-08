e552101e510b4e53569de538f522e6c37f3e57f1
Paul Davis <pcdavis@umass.edu>
2022-04-08 00:41:08 -0400

    WIP commit for dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,26 +1,31 @@
+import { getRedirectStatus } from "next/dist/lib/load-custom-routes";
 import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Dashboard.module.css"
-import clientPromise from "../lib/mongodb"
 
-// https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props
-export async function getServerSideProps(context) {
-	try {
-		// client.db() will be the default database passed in the MONGODB_URI
-		// You can change the database by calling the client.db() function and specifying a database like:
-		// const db = client.db("myDatabase");
-		// Then you can execute queries against your database like so:
-		// db.find({}) or any of the MongoDB Node Driver commands
-		await clientPromise
-		return {
-			props: { isConnected: true },
-		}
-	} catch (e) {
-		console.error(e)
-		return {
-			props: { isConnected: false },
-		}
-	}
+// function getProgress() {
+// 	return {
+// 		width: 100,
+// 		aria-valuenow: 75,
+// 		aria-valuemin: 0,
+// 		aria-valuemax: 100
+// 	};
+// }
+function getProgWidth() {
+	// TODO: some way to retrieve the width from the database here
+	let width = 0; // 1000 will be max width (100%)
+	return width > 100 ? 100: width;
+}
+
+function getName() {
+	// TODO: implement name retrieval
+	return "John Smith";
+}
+
+function getStatus() {
+	// TODO: implement retrieval of 'status': level of contribution
+	// Could be along the lines of 'super volunteer' or 'top contributor,' etc.
+	return "Status goes here";
 }
 
 export default function Dashboard({ isConnected }) {
@@ -34,8 +39,38 @@ export default function Dashboard({ isConnected }) {
 
 			<header className={styles.header}>
 				<Image className={styles.image} src="/phenomsmallwhite.webp" width="125" height="100" alt="PHENOM Logo"/>
-				<nav className={styles.headerBar}></nav>
+				<nav className={styles.headerBar}>
+					<h1 className={styles.headerText}>PHENOM Dashboard</h1>
+					<a className={styles.link} href="https://phenomonline.org/">Back to PHENOM Home</a>
+				</nav>
 			</header>
+
+			
+			<div className={styles.milestoneTracker}>
+				{/*TODO: implement labeled segments in progress bar*/}
+				<h2 className={styles.smallHeaderText}>Your Signup Milestones:</h2>
+				<div className="progress" style={{margin: "0 25px 0 25px", width: "35vw", border: "solid black 2px", flex: 1}}>
+  				<div className="progress-bar progress-bar-striped" role="progressbar" style={{width: getProgWidth() + "%", ['background-color']: "#6b076b"}} aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">{getProgWidth() + '%'}</div>
+				</div>
+			</div>
+
+			<main className={styles.main}>
+				<div className={styles.profileCard}>
+					<header className={styles.cardBanner}>
+						<h2 className={styles.bannerText}>Your Profile Info:</h2>
+					</header>
+					<div className={styles.cardRow}>
+						<h2 className={styles.smallHeaderText}>{getName()}</h2>
+						<div className={styles.statusTag}>
+							<h4 className={styles.tagText}>{getStatus()}</h4>
+						</div>
+					</div>
+				</div>
+			</main>
+
+		<footer className={styles.footer}>
+
+		</footer>
 		</div>
-	);
+		);
 }
